1722107cdbbdfa04730d04f8515799c0dcddc8ea
Роман <mr.roman.gray@gmail.com>
2018-03-03 17:22:33 +0600

    Программы для определения года рождения и времени
    Программа для Определения Года, с помощью количества лет определяет, в каком году человек родился.
    Во 2 С помощью времени определяется время суток.

---
+++ program.py
@@ -0,0 +1,9 @@
+# * Программа опроса Года * #
+ 
+
+Age = int(input(" Сколько вам лет " ))
+Month = int(input(" Введите ваш месяц " ))
+if( Month > 3):
+    print ( 2018 - Age - 1)
+else:
+    print (2018 - Age)
---
+++ program2.py
@@ -0,0 +1,16 @@
+# * Программа опроса Времени * #
+ 
+
+Time = int(input(" Сколько сайчас часов " ))
+if Time == 1 or Time == 2 or Time == 3 or Time == 4 :
+    print (" Это Ночь ")
+elif Time == 5 or Time == 6 or Time == 7 or Time == 8 or Time == 9 or Time == 10 or Time == 11:
+    print (" Это Утро ")
+elif Time == 12:
+    print (" Это Полдень ")
+elif Time == 24:
+    print (" Это Полночь ")
+elif Time == 13 or Time == 14 or Time == 15 or Time == 16 or Time == 17:
+    print (" Это День ")
+elif Time == 18 or Time == 19 or Time == 20 or Time == 21 or Time == 22 or Time == 23 :
+    print (" Это Ночь ")
