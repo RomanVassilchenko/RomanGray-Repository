0325cdb5bff15bb388b3e9c98601c14b99141bc0
RomanGray <mr.roman.gray@gmail.com>
2018-04-13 17:43:52 +0600

    ДЗ

---
+++ Python/07.04.18 Home Work/Все задания.py
@@ -0,0 +1,52 @@
+print("1 задание:")
+ABC = []
+a = input("Введите 1 число: ")
+ABC.append(a)
+a = input("Введите 2 число: ")
+ABC.append(a)
+a = input("Введите 3 число: ")
+ABC.append(a)
+Tsum = int(int(ABC[0]) + int(ABC[1]) + int(ABC[2]))
+Tsum = Tsum - int(min(ABC))
+print(Tsum)
+
+print("2 задание:")
+
+Ans = int(0)
+a = input("Сколько будет 2 + 2 ")
+if a == 4:
+    Ans = Ans + 1
+a = input("Сколько будет 3 * 22 ")
+if a == 66:
+    Ans = Ans + 1
+a = input("Сколько будет 100 + 2 ")
+if a == 102:
+    Ans = Ans + 1
+a = input("Сколько будет 2 + 2 * 2 ")
+if a == 6:
+    Ans = Ans + 1
+a = input("Сколько будет 3 + 3 * 2 ")
+if a == 9:
+    Ans = Ans + 1
+print(Ans, "Ответов и ", 5 - Ans, 'Ответов неправильно')
+
+ABC = []
+print("3 задание:")
+a = int(input("Введите число:"))
+while(a != 0):
+    ABC.append(a % 10)
+    a = a / 10
+a.reverse()
+print(a)
+
+
+
+print("4 задание:")
+a = int(input("Введите число:"))
+while(a != 0):
+    ABC.append(a % 10)
+    a = a / 10
+print(max(ABC))
+
+
+
