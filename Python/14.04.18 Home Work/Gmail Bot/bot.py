1aeba9842869f42b9a5da064adce293d350840a7
RomanGray <mr.roman.gray@gmail.com>
2018-04-27 12:54:46 +0600

    1234

--- Python/14.04.18 Home Work/Gmail Bot/bot.py
+++ Python/14.04.18 Home Work/Gmail Bot/bot.py
@@ -1,22 +1,35 @@
+import telebot
+import config
+import requests
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types = ["text"])
+def text_handler_1(message):
+    words = message.text
+    words.split() 
+    fromaddr = words[0]
+    toaddr = words[1]
+    mypass = words[2]
+    fromaddr = words[3]
+    msg['Subject'] = words[4]
+    body = words[5]
+    
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, mypass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+if __name__ == '__main__':
+    bot.polling(none_stop = True)
+
  
-fromaddr = "mr.roman.gray@gmail.com"
-toaddr = "mr.roman.gray@gmail.com"
-mypass = "Roman_Gray#1"
- 
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Привет от Романа"
- 
-body = "Это пробное сообщение"
-msg.attach(MIMEText(body, 'plain'))
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, mypass)
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
