0adbae41465e92a8447b3c91cc4160447395cd43
RomanGray <36629811+MrRomanGray@users.noreply.github.com>
2018-10-10 17:15:13 +0600

    Создал System Converter

---
+++ System Converter/ProjectD/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ProjectD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.history = new System.Windows.Forms.RichTextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.comboBoxIn = new System.Windows.Forms.ComboBox();
+            this.comboBoxOut = new System.Windows.Forms.ComboBox();
+            this.Swap = new System.Windows.Forms.Button();
+            this.convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // history
+            // 
+            this.history.Location = new System.Drawing.Point(684, 383);
+            this.history.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(349, 202);
+            this.history.TabIndex = 0;
+            this.history.Text = "";
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(933, 196);
+            this.textBoxOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(203, 22);
+            this.textBoxOut.TabIndex = 1;
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Location = new System.Drawing.Point(575, 196);
+            this.textBoxIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(203, 22);
+            this.textBoxIn.TabIndex = 2;
+            this.textBoxIn.TextChanged += new System.EventHandler(this.textBoxIn_TextChanged);
+            // 
+            // comboBoxIn
+            // 
+            this.comboBoxIn.FormattingEnabled = true;
+            this.comboBoxIn.Items.AddRange(new object[] {
+            "Двоичной",
+            "Троичной",
+            "Восьмеричной",
+            "Десятичной",
+            "Двенадцатиричной",
+            "Шестнадцатеричной"});
+            this.comboBoxIn.Location = new System.Drawing.Point(575, 106);
+            this.comboBoxIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxIn.Name = "comboBoxIn";
+            this.comboBoxIn.Size = new System.Drawing.Size(203, 24);
+            this.comboBoxIn.TabIndex = 4;
+            this.comboBoxIn.SelectedIndexChanged += new System.EventHandler(this.comboBoxIn_SelectedIndexChanged);
+            // 
+            // comboBoxOut
+            // 
+            this.comboBoxOut.FormattingEnabled = true;
+            this.comboBoxOut.Items.AddRange(new object[] {
+            "Двоичную",
+            "Троичную",
+            "Восьмеричную",
+            "Десятичную",
+            "Двенадцатиричную",
+            "Шестнадцатеричную"});
+            this.comboBoxOut.Location = new System.Drawing.Point(933, 106);
+            this.comboBoxOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxOut.Name = "comboBoxOut";
+            this.comboBoxOut.Size = new System.Drawing.Size(203, 24);
+            this.comboBoxOut.TabIndex = 5;
+            this.comboBoxOut.SelectedIndexChanged += new System.EventHandler(this.comboBoxOut_SelectedIndexChanged);
+            // 
+            // Swap
+            // 
+            this.Swap.BackgroundImage = global::ProjectD.Properties.Resources.swap;
+            this.Swap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Swap.Location = new System.Drawing.Point(832, 94);
+            this.Swap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Swap.Name = "Swap";
+            this.Swap.Size = new System.Drawing.Size(68, 48);
+            this.Swap.TabIndex = 3;
+            this.Swap.UseVisualStyleBackColor = true;
+            this.Swap.Click += new System.EventHandler(this.Swap_Click);
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(799, 261);
+            this.convert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(131, 28);
+            this.convert.TabIndex = 6;
+            this.convert.Text = "Конвертировать";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 991);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.comboBoxOut);
+            this.Controls.Add(this.comboBoxIn);
+            this.Controls.Add(this.Swap);
+            this.Controls.Add(this.textBoxIn);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.history);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox history;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.Button Swap;
+        private System.Windows.Forms.ComboBox comboBoxIn;
+        private System.Windows.Forms.ComboBox comboBoxOut;
+        private System.Windows.Forms.Button convert;
+    }
+}
+
---
+++ System Converter/ProjectD/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProjectD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxIn_SelectedIndexChanged( object sender, EventArgs e )
+        {
+        }
+
+        private void textBoxIn_TextChanged( object sender, EventArgs e )
+        {
+        }
+
+        private void convert_Click( object sender, EventArgs e )
+        {
+            //Двоичная
+            if (comboBoxIn.SelectedIndex == 0)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    textBoxOut.Text = "" + Num;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 2));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+            //Троичная
+            if (comboBoxIn.SelectedIndex == 1)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    textBoxOut.Text = "" + Num;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 3));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+            //Восьмеричная
+            if (comboBoxIn.SelectedIndex == 2)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    textBoxOut.Text = "" + Num;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 8));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+            //Десятиричная
+            if (comboBoxIn.SelectedIndex == 3)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 10));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 10));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 10));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    textBoxOut.Text = "" + textBoxIn.Text;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 10));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 10));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+            //Двенадцатиричная
+            if (comboBoxIn.SelectedIndex == 4)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    textBoxOut.Text = "" + Num;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 12));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+            //Шестнадцатиричная
+            if (comboBoxIn.SelectedIndex == 5)
+            {
+                if (comboBoxOut.SelectedIndex == 0)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    string Final = ToNotDecimal(Num, 2);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 1)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    string Final = ToNotDecimal(Num, 3);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 2)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    string Final = ToNotDecimal(Num, 8);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 3)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    textBoxOut.Text = "" + Num;
+                }
+                if (comboBoxOut.SelectedIndex == 4)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    string Final = ToNotDecimal(Num, 12);
+                    textBoxOut.Text = "" + Final;
+                }
+                if (comboBoxOut.SelectedIndex == 5)
+                {
+                    string Num = Convert.ToString(ToDecimal(textBoxIn.Text, 16));
+                    string Final = ToNotDecimal(Num, 16);
+                    textBoxOut.Text = "" + Final;
+                }
+            }
+        }
+
+        private string ToDecimal( string Number, int NumberSystem )
+        {
+            int Pow = textBoxIn.Text.Length - 1;
+            double result = 0;
+            for (int i = 0; i < Number.Length; i++)
+            {
+                if (Number[i] >= 65)
+                {
+                    int buffer = Number[i] - 'A' + 10;
+                    int x = Convert.ToInt32(buffer.ToString());
+                    result += x * Math.Pow(NumberSystem, Pow);
+                    Pow--;
+                }
+                else
+                {
+                    int x = Convert.ToInt32(Number[i].ToString());
+                    result += x * Math.Pow(NumberSystem, Pow);
+                    Pow--;
+                }
+            }
+            return Convert.ToString(result);
+        }
+
+        private string ToNotDecimal( string number, int NumberSystem )
+        {
+            string binarynum = "";
+            int Number = Convert.ToInt32(number);
+            while (Number != 0)
+            {
+                if (Number % NumberSystem <= 9) binarynum += Number % NumberSystem;
+                else
+                {
+                    if (Number % Number == 10) binarynum += 'A';
+                    if (Number % Number == 11) binarynum += 'B';
+                    if (Number % Number == 12) binarynum += 'C';
+                    if (Number % Number == 13) binarynum += 'D';
+                    if (Number % Number == 14) binarynum += 'E';
+                    if (Number % Number == 15) binarynum += 'F';
+                }
+                Number /= NumberSystem;
+            }
+            char[] inputarray = binarynum.ToCharArray();
+            Array.Reverse(inputarray);
+            binarynum = new string(inputarray);
+            return binarynum;
+        }
+
+        private void Swap_Click( object sender, EventArgs e )
+        {
+            if (comboBoxIn.SelectedIndex != -1 || comboBoxOut.SelectedIndex != -1)
+            {
+                int buffer = comboBoxOut.SelectedIndex;
+                comboBoxOut.SelectedIndex = comboBoxIn.SelectedIndex;
+                comboBoxIn.SelectedIndex = buffer;
+            }
+        }
+
+        private void comboBoxOut_SelectedIndexChanged( object sender, EventArgs e )
+        {
+        }
+    }
+}
